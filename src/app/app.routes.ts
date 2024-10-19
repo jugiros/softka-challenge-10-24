ecd8dae8b44863dc89cbcd739cf82affc87d5151
Juan Molina <jmolina@initiumsoft.com>
2024-10-19 15:58:02 -0500

    feat: Add create product route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component";
+import {CreateProductComponent} from "./components/create-product/create-product.component";
 
 export const routes: Routes = [
-  { path: '', component: ProductListComponent }
+  { path: '', component: ProductListComponent },
+  { path: 'create-edit', component: CreateProductComponent }
 ];

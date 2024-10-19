6ab952615db4f38c26154d2511887627cff8eb81
Juan Molina <jmolina@initiumsoft.com>
2024-10-19 15:43:36 -0500

    feat: Add environments.

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  baseUrlApi: "http://localhost:3002",
+  endpoints: {
+    getProducts: "/bp/products",
+    postProducts: "/bp/products",
+    putProducts: "/bp/products/{id}",
+    deleteProducts: "/bp/products/{id}",
+    verificationProduct: "/bp/products/verification/{id}"
+  }
+}
